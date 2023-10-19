7d67be0b769690d795aceeb4bcbb0354c512d298
DanieleCorrado <corrado_daniele@virgilio.it>
2023-10-19 14:44:40 +0200

    add Snack4

---
+++ src/org/lessons/java/Snack4.java
@@ -0,0 +1,30 @@
+package org.lessons.java;
+
+import java.util.Scanner;
+
+public class Snack4 {
+
+    // Data in input una stringa verificare se è palindroma
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter a word: ");
+        String word = scanner.nextLine().toLowerCase();
+
+        boolean isPalindrome = true;
+        for (int i = 0; i < word.length() / 2; i++) {
+            if (word.charAt(i) != word.charAt(word.length() - 1 - i)) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println("The word " + word + " is a palindrome");
+        } else {
+            System.out.println("The word " + word + " is not a palindrome");
+        }
+    }
+}
